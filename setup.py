4254290f9a8921abbe80d69c0d82ab8f506e30dc
Enrico Carlesso <carlesso@amazon.com>
2021-09-02 22:57:32 -0700

    Bumping to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="route53-ddns",
-    version="0.0.1",
+    version="0.0.2",
     author="Enrico Carlesso",
     author_email="enricocarlesso@gmail.com",
     description="A simple package that updated an AWS Route53 A record to point to the current IP",
